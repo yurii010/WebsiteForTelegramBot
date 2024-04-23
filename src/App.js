c6a77624d25f359ed5b58bf77358a0e17a5149cd
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-23 18:25:19 +0300

    +form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
   const { tg, onToggleButton } = useTelegram();
@@ -13,7 +16,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
     </div>
   );
 };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,48 @@
+import { useTelegram } from '../../hooks/useTelegram';
 import './Form.css';
+import { useEffect, useState } from 'react';
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [subject, setSubject] = useState('male');
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Send credentials!'
+        })
+    }, [])
+
+    useEffect(()=>{
+        if(!country || !city){
+            tg.MainButton.hide();
+        }else{
+            tg.MainButton.show();
+        }
+    }, [country, street])
+
+    const onChangeCountry = () => {
+        setCountry(e.target.value);
+    }
+
+    const onChangeCity = () => {
+        setCity(e.target.value);
+    }
+
+    const onChangeSubject = () => {
+        setSubject(e.target.value);
+    }
+
     return (
-        <div>
-            form
+        <div className='form'>
+            <h3>Your credentials</h3>
+            <input className='input' type="text" placeholder='Your country' value={country} onChange={onChangeCountry} />
+            <input className='input' type="text" placeholder='Your city' value={city} onChange={onChangeCity} />
+            <select className='select' value={subject} onChange={onChangeSubject}>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
         </div>
     );
 };
