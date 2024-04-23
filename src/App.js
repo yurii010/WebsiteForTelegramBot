11fa24668369e411d79cff1e510c0ebd1cb271b8
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-23 17:29:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import './App.css';
+const tg = window.Telegram.WebApp;
+function App() {
+
+  useEffect(() => {
+    tg.ready();
+  })
+
+  const onClose = () => {
+    tg.close();
+  }
+  return (
+    <div className="App">
+      <button onClick={onClose}> Close </button>
+    </div>
+  );
+};
+
+export default App;
