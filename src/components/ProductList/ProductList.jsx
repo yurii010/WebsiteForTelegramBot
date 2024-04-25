4c8651487d4e34a43aba17babf8021d4fbe9c979
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-25 15:40:23 +0300

    test

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,21 +3,12 @@ import { useTelegram } from '../../hooks/useTelegram';
 import ProductItem from '../ProductItem/ProductItem';
 import './ProductList.css';
 
-const products = [
-    { id: '1', title: 'banana', price: 100, description: 'good' },
-    { id: '2', title: 'apple', price: 200, description: 'well' },
-    { id: '3', title: 'tomato', price: 300, description: 'awesome' },
-    { id: '4', title: 'cabbage', price: 400, description: 'amazing' },
-    { id: '5', title: 'onion', price: 500, description: 'pretty' },
-    { id: '6', title: 'cucumber', price: 600, description: 'sweet' },
-    { id: '7', title: 'orange', price: 700, description: 'cool' },
-    { id: '8', title: 'peach', price: 800, description: 'nice' },
-    { id: '9', title: 'cherry', price: 900, description: 'bad' },
-    { id: '10', title: 'tangerin', price: 1000, description: 'norm' },
-]
-
 const ProductList = () => {
-    const { tg, queryId, getTotalPrice, addedItems, onAdd } = useTelegram();
+    const { tg, queryId, getTotalPrice, addedItems, onAdd, products } = useTelegram();
+
+    const onAddHandler = () => {
+        onAdd(products);
+    }
 
     const onSendData = useCallback(() => {
         const data = {
@@ -47,7 +38,7 @@ const ProductList = () => {
             {products.map(item => (
                 <ProductItem
                     product={item}
-                    onAdd={onAdd}
+                    onAdd={onAddHandler}
                     className='item'
                 />
             ))}
