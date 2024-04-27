d609b51028dde029638f0fd0c1f6cdc76acd741c
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-27 20:26:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { TelegramProvider } from './hooks/useTelegram';
 import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider value={value}>
+    <Provider store={store}>
       <TelegramProvider>
         <BrowserRouter>
           <App />
