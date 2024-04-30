bd771adaf7190c6026a607f9bff9befd684573c9
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-30 10:38:36 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -12,18 +12,19 @@ export const TelegramProvider = ({ children }) => {
 
     const tg = window.Telegram.WebApp;
     const queryId = tg.initDataUnsafe?.query_id;
+    const userLanguage = user.language_code;
 
     const products = [
-        { id: '1', title: 'Banana', price: 100, description: 'Good', image: 'https://freepngimg.com/thumb/banana/13-banana-png-image-bananas-picture-download.png' },
-        { id: '2', title: 'Apple', price: 200, description: 'Well', image: 'https://freepngimg.com/thumb/apple/9-apple-png-image.png' },
-        { id: '3', title: 'Kiwi', price: 300, description: 'Awesome', image: 'https://freepngimg.com/thumb/kiwi/3-kiwi-png-image-fruit-kiwi-png-pictures-download.png' },
-        { id: '4', title: 'Raspberry', price: 400, description: 'Amazing', image: 'https://freepngimg.com/thumb/raspberry/3-rraspberry-png-image.png' },
-        { id: '5', title: 'Blackberry', price: 500, description: 'Pretty', image: 'https://freepngimg.com/thumb/blackberry/6-2-blackberry-fruit-free-png-image.png' },
-        { id: '6', title: 'Watermelon', price: 600, description: 'Sweet', image: 'https://freepngimg.com/thumb/watermelon/4-watermelon-png-image.png' },
-        { id: '7', title: 'Orange', price: 700, description: 'Cool', image: 'https://freepngimg.com/thumb/orange/12-orange-png-image-download.png' },
-        { id: '8', title: 'Peach', price: 800, description: 'Nice', image: 'https://freepngimg.com/thumb/peach/4-peach-png-image.png' },
-        { id: '9', title: 'Cherry', price: 900, description: 'Great', image: 'https://freepngimg.com/thumb/cherry/1-red-cherry-png-image-download.png' },
-        { id: '10', title: 'Tangerin', price: 1000, description: 'Good', image: 'https://freepngimg.com/thumb/orange/5-orange-png-image-download.png' },
+        { id: '1', title: (userLanguage == 'uk' || 'ru' ? 'Банани' : 'Banana'), price: 100, description: (userLanguage == 'uk' || 'ru' ? 'Добрі' : 'Good'), image: 'https://freepngimg.com/thumb/banana/13-banana-png-image-bananas-picture-download.png' },
+        { id: '2', title: (userLanguage == 'uk' || 'ru' ? 'Яблуко' : 'Apple'), price: 200, description: (userLanguage == 'uk' || 'ru' ? 'Хороші' : 'Well'), image: 'https://freepngimg.com/thumb/apple/9-apple-png-image.png' },
+        { id: '3', title: (userLanguage == 'uk' || 'ru' ? 'Ківі' : 'Kiwi'), price: 300, description: (userLanguage == 'uk' || 'ru' ? 'Приголовшливі' : 'Awesome'), image: 'https://freepngimg.com/thumb/kiwi/3-kiwi-png-image-fruit-kiwi-png-pictures-download.png' },
+        { id: '4', title: (userLanguage == 'uk' || 'ru' ? 'Малина' : 'Raspberry'), price: 400, description: (userLanguage == 'uk' || 'ru' ? 'Дивовижна' : 'Amazing'), image: 'https://freepngimg.com/thumb/raspberry/3-rraspberry-png-image.png' },
+        { id: '5', title: (userLanguage == 'uk' || 'ru' ? 'Чорниця' : 'Blackberry'), price: 500, description: (userLanguage == 'uk' || 'ru' ? 'Прекрасний' : 'Pretty'), image: 'https://freepngimg.com/thumb/blackberry/6-2-blackberry-fruit-free-png-image.png' },
+        { id: '6', title: (userLanguage == 'uk' || 'ru' ? 'Кавун' : 'Watermelon'), price: 600, description: (userLanguage == 'uk' || 'ru' ? 'Солодкий' : 'Sweet'), image: 'https://freepngimg.com/thumb/watermelon/4-watermelon-png-image.png' },
+        { id: '7', title: (userLanguage == 'uk' || 'ru' ? 'Апельсин' : 'Orange'), price: 700, description: (userLanguage == 'uk' || 'ru' ? 'Крутий' : 'Cool'), image: 'https://freepngimg.com/thumb/orange/12-orange-png-image-download.png' },
+        { id: '8', title: (userLanguage == 'uk' || 'ru' ? 'Персик' : 'Peach'), price: 800, description: (userLanguage == 'uk' || 'ru' ? 'Файні' : 'Nice'), image: 'https://freepngimg.com/thumb/peach/4-peach-png-image.png' },
+        { id: '9', title: (userLanguage == 'uk' || 'ru' ? 'Вишня' : 'Cherry'), price: 900, description: (userLanguage == 'uk' || 'ru' ? 'Чудові' : 'Great'), image: 'https://freepngimg.com/thumb/cherry/1-red-cherry-png-image-download.png' },
+        { id: '10', title: (userLanguage == 'uk' || 'ru' ? 'Мандарин' : 'Tangerin'), price: 1000, description: (userLanguage == 'uk' || 'ru' ? 'Добрі' : 'Good'), image: 'https://freepngimg.com/thumb/orange/5-orange-png-image-download.png' },
     ];
 
     /* Methods */
@@ -86,7 +87,7 @@ export const TelegramProvider = ({ children }) => {
         onSendData,
         onClose,
         onAdd,
-        };
+    };
 
     return (
         <TelegramContext.Provider value={contextValue}>
