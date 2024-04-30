699ddfd642d53874eda6539b065e184384be7406
Yurii <125599335+yurii010@users.noreply.github.com>
2024-04-30 11:05:44 +0300

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,8 +7,7 @@ const Form = () => {
     const { tg, userLanguage } = useTelegram();
     const [country, setCountry] = useState('');
     const [city, setCity] = useState('');
-    const [subject, setSubject] = useState('male');
-    //userLanguage == 'uk' || 'ru' ? 'чоловіча' : 'male'
+    const [subject, setSubject] = useState(userLanguage == 'uk' || 'ru' ? 'чоловіча' : 'male');
 
     const onSendData = useCallback(() => {
         const data = {
@@ -53,23 +52,16 @@ const Form = () => {
         setSubject(e.target.value);
     }
 
-    //{userLanguage == 'uk' || 'ru' ? 'Ваші облікові записи' : 'Your credentials'}
-    //userLanguage == 'uk' || 'ru' ? 'Ваша країна' : 'Your country'}
-    //{userLanguage == 'uk' || 'ru' ? 'Ваше місто' : 'Your city'}
-
     return (
         <div className='form mt'>
-            <h3 className='title-form'>Your credentials</h3>
-            <input className='input-form' type="text" placeholder={'Your country'} value={country} onChange={onChangeCountry} />
-            <input className='input-form' type="text" placeholder={'Your city'} value={city} onChange={onChangeCity} />
+            <h3 className='title-form'>{userLanguage == 'uk' || 'ru' ? 'Ваші облікові записи' : 'Your credentials'}</h3>
+            <input className='input-form' type="text" placeholder={userLanguage == 'uk' || 'ru' ? 'Ваша країна' : 'Your country'} value={country} onChange={onChangeCountry} />
+            <input className='input-form' type="text" placeholder={userLanguage == 'uk' || 'ru' ? 'Ваше місто' : 'Your city'} value={city} onChange={onChangeCity} />
             <select className='select-form' value={subject} onChange={onChangeSubject}>
-                <option value={"male"}>Male</option>
-                <option value={"female"}>Female</option>
+                <option value={"male"}>{userLanguage == 'uk' || 'ru' ? 'Чоловік' : 'Male'}</option>
+                <option value={"female"}>{userLanguage == 'uk' || 'ru' ? 'Жінка' : 'Female'}</option>
             </select>
         </div>
     );
 };
 export default Form;
-
-//{userLanguage == 'uk' || 'ru' ? 'Чоловік' : 'Male'}
-//{userLanguage == 'uk' || 'ru' ? 'Жінка' : 'Female'}
