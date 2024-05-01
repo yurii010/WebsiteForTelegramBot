f14ea57b4e13db64be786b2dbd2232cc29a75dca
Yurii <125599335+yurii010@users.noreply.github.com>
2024-05-01 11:43:05 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,27 +6,14 @@ import { useTelegram } from '../hooks/useTelegram';
 
 const Header = () => {
     const { onClose, user, userLanguage, userId } = useTelegram();
-    const [userLang, setUserLang] = useState('');
-
-    useEffect(() => {
-        const fetchUserLanguage = async () => {
-            try {
-                const response = await fetch(`https://39c4-217-196-161-98.ngrok-free.app/user-language?userId=${userId}`);
-                const data = await response.json();
-                setUserLang(data.languageCode);
-            } catch (e) {
-                console.error(e);
-            }
-        };
-        fetchUserLanguage();
-    }, [userId]);
+    //const [userLang, setUserLang] = useState('');
 
     return (
         <div className='header'>
             <Button className="close-button" onClick={onClose}>{userLanguage == 'uk' || 'ru' ? 'Закрити' : 'Close'}</Button>
             <span className='username'>
                 {userLanguage == 'uk' || 'ru' ? 'Ваш нікнейм: ' : 'Your username: '}{user?.username}<br />
-                Test work?: {userLang}
+                Test work?: {userId}
             </span>
         </div>
     );
