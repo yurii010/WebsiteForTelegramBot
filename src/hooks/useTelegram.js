3cdd108e96a6928a4e3cd51c5c3372d35a00cbaa
Yurii <125599335+yurii010@users.noreply.github.com>
2024-05-09 00:11:20 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -87,7 +87,7 @@ export const TelegramProvider = ({ children }) => {
             const { userL } = response.data;
             setUserLang(userL);
         } catch (error) {
-            return
+            console.error("Error fetching user language", error);
         }
     };
 
