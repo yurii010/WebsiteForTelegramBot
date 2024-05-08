94b6daacbd39bb83bdf5ae5c4ea7b6d12e2ef455
Yurii <125599335+yurii010@users.noreply.github.com>
2024-05-08 15:10:51 +0300

    some fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
     const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        if (token) {
+            return
+        } else {
+            navigate("/auth/login");
+        }
+    }, [token])
 
     const handleLogout = () => {
         localStorage.removeItem('token');
