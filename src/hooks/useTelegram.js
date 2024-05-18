fa1bb459b38c8aac1e2162cfcfaf8b4778afe592
Yurii <125599335+yurii010@users.noreply.github.com>
2024-05-18 22:12:04 +0300

    new link

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -12,7 +12,7 @@ export const TelegramProvider = ({ children }) => {
 
     /* Properties */
 
-    const link = "https://a889-217-196-161-98.ngrok-free.app";
+    const link = "https://b807-217-196-161-98.ngrok-free.app";
 
     const tg = window.Telegram.WebApp;
     const queryId = tg.initDataUnsafe?.query_id;
